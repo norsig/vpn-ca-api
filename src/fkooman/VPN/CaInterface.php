2fe9168d10467203e3fac22aae2aaf95b66e302d
François Kooman <fkooman@tuxed.net>
2015-11-28 23:20:43 +0100

    update interface documentation

--- src/fkooman/VPN/CaInterface.php
+++ src/fkooman/VPN/CaInterface.php
@@ -19,15 +19,68 @@ namespace fkooman\VPN;
 
 interface CaInterface
 {
+    /**
+     * Generate a certificate for the VPN server.
+     * 
+     * @param string $commonName
+     *
+     * @return array the certificate and key in array with keys 'cert' and
+     *               'key'
+     */
     public function generateServerCert($commonName);
+
     public function generateDh();
+
+    /**
+     * Generate a certificate for a VPN client.
+     * 
+     * @param string $commonName
+     *
+     * @return array the certificate and key in array with keys 'cert' and
+     *               'key'
+     */
     public function generateClientCert($commonName);
+
     public function getTlsAuthKey();
+
+    /** 
+     * Check if the CA already issued a certificate.
+     *
+     * @param string $commonName
+     *
+     * @return bool whether or not the commonName already has a certificate
+     */
     public function hasCert($commonName);
+
+    /**
+     * Get the CA root certificate.
+     */
     public function getCaCert();
+
+    /**
+     * Get the CA CRL.
+     */
     public function getCrl();
+
+    /**
+     * Get the CA CRL last modified time.
+     *
+     * @return string
+     */
     public function getCrlLastModifiedTime();
+
+    /**
+     * Get the file size of the CA CRL.
+     */
     public function getCrlFileSize();
+
+    /**
+     * Revoke a VPN client certificate.
+     */
     public function revokeClientCert($commonName);
+
+    /**
+     * Initialize the CA.
+     */
     public function initCa();
 }
